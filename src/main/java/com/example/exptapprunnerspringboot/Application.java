b1a6b8cec1ad91de71c64a0ec2f25c79c0bb741e
kaito <kaito.develop@gmail.com>
2024-11-09 22:13:59 +0900

    Add Redis

--- src/main/java/com/example/exptapprunnerspringboot/Application.java
+++ src/main/java/com/example/exptapprunnerspringboot/Application.java
@@ -11,6 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+    // RedisTemplate を Inject
+    private final RedisTemplate<String, String> redisTemplate;
+
+    Application(final RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -18,6 +25,28 @@ public class Application {
     @GetMapping("/")
     public String root() {
         return "hello!! It's knagai's spring application!!\n";
+    }
+
+
+    // 以下3つの Mapping を追加
+    @GetMapping("/health")
+    public String getHealth() {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        return "Status: " + ops.get("health");
+    }
+
+    @GetMapping("/healthy")
+    public String setHealty() {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        ops.set("health", "healthy");
+        return "Status: " + ops.get("health");
+    }
+
+    @GetMapping("/unhealthy")
+    public String setUnhealty() {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        ops.set("health", "unhealthy");
+        return "Status: " + ops.get("health");
     }
 
 }
